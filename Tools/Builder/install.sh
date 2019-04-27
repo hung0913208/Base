9ff934b62ece10ada72d4ffb1ef29eb486b0d519
Pham Nguyen Xuan Hung <hung0913208@gmail.com>
2019-04-28 06:32:29 +0700

    [FIX][Tools/Builder] branch Tests can't run
    Problems:
    - Script Tools/Builder/install.sh is designed to install on branch
    Tools/Builder only and we can't use it on branch Tests recently
    
    Solutions:
    - Temporary build Libraries only and this commit will be revert soon

--- Tools/Builder/install.sh
+++ Tools/Builder/install.sh
@@ -41,15 +41,8 @@ mkdir -p $ROOT/build
 # @NOTE: jump and perform building now
 cd $ROOT/build || error "cannot jump to $ROOT/build"
 
-# @NOTE: migrate Eevee to subproject Builder
-cp -a $ROOT/Libraries $ROOT/Tools/Builder || error "cannot deliver $ROOT/Libraries to $ROOT/Tools/Builder"
-cp -a $ROOT/Tests $ROOT/Tools/Builder  || error "cannot deliver $ROOT/Tests to $ROOT/Tools/Builder"
-
-# @NOTE: jump and perform building now
-cd $ROOT/Tools/Builder || error "cannot jump to $ROOT/Tools/Builder"
-
 # @NOTE: verify libraries that are used during install Builder 2
-info "reinstall Builder 2 on Debug mode"
+info "reinstall Libraries on Debug mode"
 
 # @NOTE: disable stdout recently, we don't want to taint our console with unexpected messages
 exec 2>&-
@@ -66,11 +59,11 @@ else
 	$ROOT/Tools/Utilities/ctest.sh Debug
 	if [ $? != 0 ]; then
 		warning "fail test on Debug mode -> remove this candidate"
-		rm -fr $ROOT/Tools/Builder/Debug
+		rm -fr $ROOT/build/Debug
 	fi
 fi
 
-info "reinstall Builder 2 on Release mode"
+info "reinstall Libraries on Release mode"
 $ROOT/Tools/Utilities/reinstall.sh Release >& /dev/null
 
 if [ $? != 0 ]; then
@@ -79,17 +72,10 @@ else
 	$ROOT/Tools/Utilities/ctest.sh Release
 	if [ $? != 0 ]; then
 		warning "fail test on Release mode -> remove this candidate"
-		rm -fr $ROOT/Tools/Builder/Debug
+		rm -fr $ROOT/build/Release
 	fi
 fi
 
-if [ -x $ROOT/Tools/Builder/Release/build2 ]; then
-	ln -s $ROOT/Tools/Builder/Release/build2 $ROOT/Tools/Builder/build2
-elif [ -x $ROOT/Tools/Builder/Debug/build2 ]; then
-	ln -s $ROOT/Tools/Builder/Debug/build2 $ROOT/Tools/Builder/build2
-else
-	error "fail to install Builder 2"
-fi
 # @NOTE: back again
 cd $CURRENT || error "cannot jump to $CURRENT"
 
