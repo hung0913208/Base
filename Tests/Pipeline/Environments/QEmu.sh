1f904fe93aa469f8248b78dfe57793ab73f65812
Pham Nguyen Xuan Hung <hung0913208@gmail.com>
2019-05-01 10:53:36 +0700

    [ENHANCE][Tests/Pipeline] fetch latest pipelines

--- Tests/Pipeline/Environments/QEmu.sh
+++ Tests/Pipeline/Environments/QEmu.sh
@@ -1,7 +1,54 @@
 #!/bin/bash
 
-ROOT="$(git rev-parse --show-toplevel)"
+REPO=$3
+METHOD=$1
+BRANCH=$4
+PIPELINE=$2
+
+source $PIPELINE/Libraries/Logcat.sh
+source $PIPELINE/Libraries/Package.sh
 
 SCRIPT="$(basename "$0")"
-PROJECT="$(basename `git config  --get remote.origin.url`)"
 
+git branch | egrep 'Pipeline/QEmu$' >& /dev/null
+if [ $? != 0 ]; then
+	CURRENT=$(pwd)
+
+	cd $PIPELINE || error "can't cd to $PIPELINE"
+	git branch -a | egrep 'remotes/origin/Pipeline/QEmu$' >& /dev/null
+
+	if [ $? = 0 ]; then
+		# @NOTE: by default we will fetch the maste branch of this environment
+		# to use the best candidate of the branch
+
+		git fetch >& /dev/null
+		git checkout -b "Pipeline/QEmu" "origin/Pipeline/QEmu"
+		if [ $? = 0 ]; then
+			"$0" $METHOD $PIPELINE $REPO $BRANCH
+		fi
+
+		exit $?
+	fi
+
+	cd $CURRENT || error "can't cd to $CURRENT"
+fi
+
+# @NOTE: otherwide, we should the current environment since the branch maybe
+# merged with the master
+if [ "$METHOD" == "reproduce" ]; then
+	$PIPELINE/Reproduce.sh $REPO
+	exit $?
+elif [[ $# -gt 3 ]]; then
+	BRANCH=$4
+	info "import '$REPO $BRANCH' >> ./repo.list"
+
+	echo "$REPO $BRANCH" >> ./repo.list
+else
+	# @NOTE: fetch the list project we would like to build from remote server
+	if [ ! -f './repo.list' ]; then
+		curl -k --insecure $REPO -o './repo.list' >> /dev/null
+		if [ $? != 0 ]; then
+			error "Can't fetch list of project from $REPO"
+		fi
+	fi
+fi
--- Tests/Pipeline/Environments/Traditional.sh
+++ Tests/Pipeline/Environments/Traditional.sh
@@ -3,20 +3,45 @@
 # - Description: since we are working on traditional way we must prepare our
 # system according to the list of project we would like to build now
 
-METHOD=$1
-PIPELINE=$2
 REPO=$3
+METHOD=$1
+BRANCH=$4
+PIPELINE=$2
 
 source $PIPELINE/Libraries/Logcat.sh
 source $PIPELINE/Libraries/Package.sh
 
 SCRIPT="$(basename "$0")"
 
+git branch | egrep 'Pipeline/Traditional$'
+if [ $? != 0 ]; then
+	CURRENT=$(pwd)
+
+	cd $PIPELINE || error "can't cd to $PIPELINE"
+	git branch -a | egrep 'remotes/origin/Pipeline/Traditional$'
+
+	if [ $? = 0 ]; then
+		# @NOTE: by default we will fetch the maste branch of this environment
+		# to use the best candidate of the branch
+
+		git fetch >& /dev/null
+		git checkout -b "Pipeline/Traditional" "origin/Pipeline/Traditional"
+		if [ $? = 0 ]; then
+			"$0" $METHOD $PIPELINE $REPO $BRANCH
+		fi
+
+		exit $?
+	fi
+
+	cd $CURRENT || error "can't cd to $CURRENT"
+fi
+
+# @NOTE: otherwide, we should the current environment since the branch maybe
+# merged with the master
 if [ "$METHOD" == "reproduce" ]; then
 	$PIPELINE/Reproduce.sh $REPO
 	exit $?
 elif [[ $# -gt 3 ]]; then
-	BRANCH=$4
 	info "import '$REPO $BRANCH' >> ./repo.list"
 
 	echo "$REPO $BRANCH" >> ./repo.list
