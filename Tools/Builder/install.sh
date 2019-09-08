ddcfecb9bd25f5279d1c81a916c6651ba52df876
Pham Nguyen Xuan Hung <hung0913208@gmail.com>
2019-09-08 21:32:18 +0700

    [REVERT][Tools/Builder] branch Tests can't run
    This reverts commit d1f7f85be820efda19f932862a1ff0fc578b9f4f.
    
    Change-Id: Ibdbc898ae2a7d8c9812e40afe438764f29eb6f25

--- Tools/Builder/install.sh
+++ Tools/Builder/install.sh
@@ -36,6 +36,72 @@ git submodule update --init
 ROOT="$(git rev-parse --show-toplevel)"
 CURRENT=$(pwd)
 
+mkdir -p $ROOT/build
+
+# @NOTE: jump and perform building now
+cd $ROOT/build || error "cannot jump to $ROOT/build"
+
+# @NOTE: migrate Eevee to subproject Builder
+cp -a $ROOT/Libraries $ROOT/Tools/Builder || error "cannot deliver $ROOT/Libraries to $ROOT/Tools/Builder"
+cp -a $ROOT/Tests $ROOT/Tools/Builder  || error "cannot deliver $ROOT/Tests to $ROOT/Tools/Builder"
+
+# @NOTE: jump and perform building now
+cd $ROOT/Tools/Builder || error "cannot jump to $ROOT/Tools/Builder"
+
+# @NOTE: verify libraries that are used during install Builder 2
+info "reinstall Builder 2 on Debug mode"
+
+# @NOTE: disable stdout recently, we don't want to taint our console with unexpected messages
+exec 2>&-
+OUTPUT="$($ROOT/Tools/Utilities/reinstall.sh Debug 2>&1 | tee /dev/fd/2)"
+
+if [[ $(grep "make\: \*\*\* \[all\] Error" <<< "$OUTPUT" ) ]]; then
+	warning "fail build on Debug mode, please check console log bellow"
+	echo "================================================================================="
+	echo ""
+	echo "$OUTPUT"
+	echo "---------------------------------------------------------------------------------"
+	exit -1
+else
+	$ROOT/Tools/Utilities/ctest.sh Debug
+	if [ $? != 0 ]; then
+		warning "fail test on Debug mode -> remove this candidate"
+		rm -fr $ROOT/Tools/Builder/Debug
+	fi
+fi
+
+info "reinstall Builder 2 on Release mode"
+#exec 2>&-
+OUTPUT="$($ROOT/Tools/Utilities/reinstall.sh Release 2>&1 | tee /dev/fd/2)"
+
+if [[ $(grep "make\: \*\*\* \[all\] Error" <<< "$OUTPUT" ) ]]; then
+	warning "fail build on Release mode"
+	echo "================================================================================="
+	echo ""
+	echo "$OUTPUT"
+	echo "---------------------------------------------------------------------------------"
+	exit -1
+else
+	$ROOT/Tools/Utilities/ctest.sh Release
+	if [ $? != 0 ]; then
+		warning "fail test on Release mode -> remove this candidate"
+		rm -fr $ROOT/Tools/Builder/Debug
+	fi
+fi
+
+if [ -x $ROOT/Tools/Builder/Release/build2 ]; then
+	ln -s $ROOT/Tools/Builder/Release/build2 $ROOT/Tools/Builder/build2
+elif [ -x $ROOT/Tools/Builder/Debug/build2 ]; then
+	ln -s $ROOT/Tools/Builder/Debug/build2 $ROOT/Tools/Builder/build2
+else
+	warning "fail to install Builder 2"
+	echo "================================================================================="
+	echo ""
+	echo "$OUTPUT"
+	echo "---------------------------------------------------------------------------------"
+	exit -1
+fi
+
 # @NOTE: back again
 cd $CURRENT || error "cannot jump to $CURRENT"
 
