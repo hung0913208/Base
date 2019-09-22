2aafa399df113913ff8c2cd8a9807aa8556d2033
Pham Nguyen Xuan Hung <hung0913208@gmail.com>
2019-09-22 07:22:29 +0700

    [ENHANCE][GitlabCI] detect which patch is freed
    Signed-off-by: Pham Nguyen Xuan Hung <hung0913208@gmail.com>
    Change-Id: Iba99f78776534d7bcf2fbb076e4009368111726d

--- .gitlab-ci.d/travis/build.sh
+++ .gitlab-ci.d/travis/build.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
 for JOB in $(python -c "for v in '$1'.split(';'): print(v)"); do
-	if ! ./Tools/Utilities/travis.sh restart --job ${JOB} --patch ${PATCH} --token ${TOKEN} --repo ${REPO}; then
-		exit -1
+	RUN=0
+
+	for IDX in `seq ${BEGIN} ${END}`; do
+		STATUS=$(./Tools/Utilities/travis.sh status --job ${JOB} --patch ${IDX} --token ${TOKEN} --repo ${REPO})
+
+		if [ $STATUS != 'started' ]; then
+			RUN=1
+
+			if ! ./Tools/Utilities/travis.sh restart --job ${JOB} --patch ${IDX} --token ${TOKEN} --repo ${REPO}; then
+				exit -1
+			else
+				break
+			fi
+		fi
+	done
+
+	if [[ $RUN -eq 0 ]]; then
+		echo "[  ERROR  ]: Node so busy to perform your task, please try again later or expand the range"
+		exit -2
 	fi
 done
