855996ef7d22eb4a80d0ca05a82716cc9d0a0b6d
Pham Nguyen Xuan Hung <hung0913208@gmail.com>
2019-09-25 21:16:36 +0700

    [FIX][GitlabCI] fix fall-throught configuration
    Problems:
    - Many pipeline uses the same commit during testing even if we are
    requesting on different commit. This is caused by applying the selecting
    rule so weak and causes some tasks choose the wrong configuration.
    
    Solution:
    - Enhance the logic to improve the selecting job better.
    
    Signed-off-by: Pham Nguyen Xuan Hung <hung0913208@gmail.com>
    Change-Id: I7f0e440320ac968d3cc181066022df0ad5d5c67e
    Signed-off-by: Pham Nguyen Xuan Hung <hung0913208@gmail.com>

--- .gitlab-ci.d/travis/build.sh
+++ .gitlab-ci.d/travis/build.sh
@@ -7,10 +7,10 @@ for JOB in $(python -c "for v in '$1'.split(';'): print(v)"); do
 		STATUS=$(./Tools/Utilities/travis.sh status --job ${JOB} --patch ${IDX} --token ${TOKEN} --repo ${REPO})
 		CODE=0
 
-		if [ $STATUS != 'started' ]; then
+		if [ $STATUS = 'passed' ] || [ $STATUS = 'failed' ] || [ $STATUS = 'canceled' ] || [ $STATUS = 'errored' ]; then
 			RUN=1
 			HOOK="HOOK${JOB}_gitlab_${CI_JOB_ID}"
-			VALUE="\\\"if [[ \\\\\$ID -eq $JOB ]]; then export JOB='build'; echo '${CI_REPOSITORY_URL} ${CI_COMMIT_REF_NAME}' >> ./repo.list; ADOPTED=1; fi\\\""
+			VALUE="\\\"if [ \\\\\$TRAVIS_JOB_NUMBER = '$IDX.$JOB' ]; then export JOB='build'; echo '${CI_REPOSITORY_URL} ${CI_COMMIT_REF_NAME}' >> ./repo.list; ADOPTED=1; fi\\\""
 
 			./Tools/Utilities/travis.sh env add --name "$HOOK" --value "$VALUE" --token ${TOKEN} --repo ${REPO}			
 			if ! ./Tools/Utilities/travis.sh restart --job ${JOB} --patch ${IDX} --token ${TOKEN} --repo ${REPO}; then
