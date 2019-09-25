e5ea1925acb00d683f0bdd2386e511b7c5438d59
Pham Nguyen Xuan Hung <hung0913208@gmail.com>
2019-09-25 22:10:05 +0700

    [ENHANCE][GitlabCI] build with specific repo
    Signed-off-by: Pham Nguyen Xuan Hung <hung0913208@gmail.com>
    Change-Id: I22592fc6b89912ee1a0813c94c3ab4968e855d83

--- .gitlab-ci.d/travis/kernel.sh
+++ .gitlab-ci.d/travis/kernel.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+if [[ ${#REPOSITORY} -gt 0 ]] && [[ ${#BRANCH} -gt 0 ]]; then
+	if [[ ${#USERNAME} -gt 0 ]] && [[ ${#PASSWORD} -gt 0 ]]; then
+		PROTOCOL=$(python -c "print('${REPOSITORY}'.split('://')[0])")
+		REPOSITORY=$(python -c "print('${REPOSITORY}'.split('://')[1])")
+		REPOSITORY="${PROTOCOL}://$USERNAME:$PASSWORD@$REPOSITORY"
+	fi
+else
+	REPOSITORY=${CI_REPOSITORY_URL}
+	BRANCH=${CI_COMMIT_REF_NAME}
+fi
+
 for JOB in $(python -c "for v in '$1'.split(';'): print(v)"); do
 	RUN=0
 
@@ -10,7 +21,7 @@ for JOB in $(python -c "for v in '$1'.split(';'): print(v)"); do
 		if [ $STATUS = 'passed' ] || [ $STATUS = 'failed' ] || [ $STATUS = 'canceled' ] || [ $STATUS = 'errored' ]; then
 			RUN=1
 			HOOK="HOOK${JOB}_gitlab_${CI_JOB_ID}"
-			VALUE="\\\"if [ \\\\\$TRAVIS_JOB_NUMBER = '$IDX.$JOB' ]; then export JOB='build'; sudo apt install qemu; echo '${CI_REPOSITORY_URL} ${CI_COMMIT_REF_NAME}' >> ./repo.list; ADOPTED=1; fi\\\""
+			VALUE="\\\"if [ \\\\\$TRAVIS_JOB_NUMBER = '$IDX.$JOB' ]; then export JOB='build'; sudo apt install qemu; echo '$REPOSITORY $BRANCH' >> ./repo.list; ADOPTED=1; fi\\\""
 
 			./Tools/Utilities/travis.sh env add --name "$HOOK" --value "$VALUE" --token ${TOKEN} --repo ${REPO}
 			if ! ./Tools/Utilities/travis.sh restart --job ${JOB} --patch ${IDX} --token ${TOKEN} --repo ${REPO}; then
--- .gitlab-ci.d/travis/reproduce.sh
+++ .gitlab-ci.d/travis/reproduce.sh
@@ -4,6 +4,17 @@ if [ ! -f ./Tests/Pipeline/Fetch.sh ]; then
 	exit 0
 fi
 
+if [[ ${#REPOSITORY} -gt 0 ]] && [[ ${#BRANCH} -gt 0 ]]; then
+	if [[ ${#USERNAME} -gt 0 ]] && [[ ${#PASSWORD} -gt 0 ]]; then
+		PROTOCOL=$(python -c "print('${REPOSITORY}'.split('://')[0])")
+		REPOSITORY=$(python -c "print('${REPOSITORY}'.split('://')[1])")
+		REPOSITORY="${PROTOCOL}://$USERNAME:$PASSWORD@$REPOSITORY"
+	fi
+else
+	REPOSITORY=${CI_REPOSITORY_URL}
+	BRANCH=${CI_COMMIT_REF_NAME}
+fi
+
 for ITEM in $(python -c "for v in '$1'.split(';'): print(v)"); do
 	RUN=0
 	JOB=$(python -c "print('$ITEM'.split(':')[0])")
@@ -16,7 +27,7 @@ for ITEM in $(python -c "for v in '$1'.split(';'): print(v)"); do
 		if [ $STATUS = 'passed' ] || [ $STATUS = 'failed' ] || [ $STATUS = 'canceled' ] || [ $STATUS = 'errored' ]; then
 			RUN=1
 			HOOK="HOOK${JOB}_gitlab_${CI_JOB_ID}"
-			VALUE="\\\"if [ \\\\\$TRAVIS_JOB_NUMBER = '$IDX.$JOB' ]; then export JOB='reproduce'; echo '$ISSUE 100000 ${CI_REPOSITORY_URL} ${CI_COMMIT_REF_NAME} ${FTP}' >> ./repo.list; ADOPTED=1; fi\\\""
+			VALUE="\\\"if [ \\\\\$TRAVIS_JOB_NUMBER = '$IDX.$JOB' ]; then export JOB='reproduce'; echo '$ISSUE 100000 $REPOSITORY $BRANCH ${FTP}' >> ./repo.list; ADOPTED=1; fi\\\""
 
 			./Tools/Utilities/travis.sh env add --name "$HOOK" --value "$VALUE" --token ${TOKEN} --repo ${REPO}
 			if ! ./Tools/Utilities/travis.sh restart --job ${JOB} --patch ${IDX} --token ${TOKEN} --repo ${REPO}; then
