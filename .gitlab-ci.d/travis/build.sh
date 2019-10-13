2a272ce61a6e014d29cc0661e49dfb50571cd8a8
Pham Nguyen Xuan Hung <hung0913208@gmail.com>
2019-10-13 22:51:15 +0700

    [ENHANCE][Tests/Pipeline] test with muti-env
    Description:
    - Since we apply backword compatible with old version's gcc, we should
    test our code to verify which versions are supporting to help to fix
    corner issues
    
    Change-Id: I6cb5e93d9d4d2fc08caad1f00d3db4eb9a91d6c3
    Signed-off-by: Pham Nguyen Xuan Hung <hung0913208@gmail.com>

--- .gitlab-ci.d/travis/build.sh
+++ .gitlab-ci.d/travis/build.sh
@@ -11,12 +11,13 @@ else
 	BRANCH=${CI_COMMIT_REF_NAME}
 fi
 
+CODE=0
+
 for JOB in $(python -c "for v in '$1'.split(';'): print(v)"); do
 	RUN=0
 
 	for IDX in `seq ${BEGIN} ${END}`; do
 		STATUS=$(./Tools/Utilities/travis.sh status --job ${JOB} --patch ${IDX} --token ${TRAVIS} --repo ${REPO})
-		CODE=0
 
 		if [ $STATUS = 'passed' ] || [ $STATUS = 'failed' ] || [ $STATUS = 'canceled' ] || [ $STATUS = 'errored' ]; then
 			START="HOOK${JOB}_gitlab_${CI_JOB_ID}"
@@ -36,24 +37,12 @@ EOF
 			./Tools/Utilities/travis.sh env add --name "$START" --value "$HOOK" --token ${TRAVIS} --repo ${REPO}
 			./Tools/Utilities/travis.sh env add --name "$STOP" --value "$NOTIFY" --token ${TRAVIS} --repo ${REPO}
 
+			RUN=1
 			if ! ./Tools/Utilities/travis.sh restart --job ${JOB} --patch ${IDX} --token ${TRAVIS} --repo ${REPO}; then
 				CODE=-1
 			fi
 
-			if ! ./Tools/Utilities/travis.sh env del --name "$START" --token ${TRAVIS} --repo ${REPO}; then
-				RUN=0
-			elif ! ./Tools/Utilities/travis.sh env del --name "$STOP" --token ${TRAVIS} --repo ${REPO}; then
-
-				RUN=0
-			else
-				RUN=1
-			fi
-
-			if [ $CODE != 0 ]; then
-				exit $CODE
-			else
-				break
-			fi
+			break
 		fi
 	done
 
@@ -62,3 +51,5 @@ EOF
 		exit -2
 	fi
 done
+
+exit $CODE
--- .gitlab-ci.d/travis/kernel.sh
+++ .gitlab-ci.d/travis/kernel.sh
@@ -11,12 +11,12 @@ else
 	BRANCH=${CI_COMMIT_REF_NAME}
 fi
 
+CODE=0
 for JOB in $(python -c "for v in '$1'.split(';'): print(v)"); do
 	RUN=0
 
 	for IDX in `seq ${BEGIN} ${END}`; do
 		STATUS=$(./Tools/Utilities/travis.sh status --job ${JOB} --patch ${IDX} --token ${TRAVIS} --repo ${REPO})
-		CODE=0
 
 		if [ $STATUS = 'passed' ] || [ $STATUS = 'failed' ] || [ $STATUS = 'canceled' ] || [ $STATUS = 'errored' ]; then
 			START="HOOK${JOB}_gitlab_${CI_JOB_ID}"
@@ -35,23 +35,12 @@ EOF
 
 			./Tools/Utilities/travis.sh env add --name "$START" --value "$HOOK" --token ${TRAVIS} --repo ${REPO}
 			./Tools/Utilities/travis.sh env add --name "$STOP" --value "$NOTIFY" --token ${TRAVIS} --repo ${REPO}
+
+			RUN=1
 			if ! ./Tools/Utilities/travis.sh restart --job ${JOB} --patch ${IDX} --token ${TRAVIS} --repo ${REPO}; then
 				CODE=-1
-			fi
-
-			if ! ./Tools/Utilities/travis.sh env del --name "$START" --token ${TRAVIS} --repo ${REPO}; then
-				RUN=0
-			elif ! ./Tools/Utilities/travis.sh env del --name "$STOP" --token ${TRAVIS} --repo ${REPO}; then
-
-				RUN=0
-			else
-				RUN=1
-			fi
-			if [ $CODE != 0 ]; then
-				exit $CODE
-			else
-				break
-			fi
+			fi	
+			break
 		fi
 	done
 
@@ -60,3 +49,5 @@ EOF
 		exit -2
 	fi
 done
+
+exit $CODE
