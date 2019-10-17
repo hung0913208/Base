79a20ccec48df473dbe16cecb95f147b480721c0
Pham Nguyen Xuan Hung <hung0913208@gmail.com>
2019-10-18 05:32:38 +0700

    [UPGRADE][Tests/Wrapping] improve test Parameters
    Description:
    - Since we have more feature, the test case should expand to cover
    everything.
    
    Change-Id: I94bf0a4ca2697e8005b9bd0259f9bcbb4a141413
    Signed-off-by: Pham Nguyen Xuan Hung <hung0913208@gmail.com>

--- Tests/Wrapping/Function.cc
+++ Tests/Wrapping/Function.cc
@@ -2,8 +2,9 @@
 #include <Unittest.h>
 
 using namespace Base;
+static Bool EnteredPrint = False;
 
-Void Print(void) {
+extern "C" Void Print(void) {
   String console, buffer;
   Stream stream{[&](Bytes&& buffer, UInt buffer_size) -> ErrorCodeE {
     console += String{(char*)buffer, buffer_size};
@@ -28,9 +29,12 @@ Void Print(void) {
 
   stream >> buffer;
   EXPECT_EQ(buffer, "test");
+
+  EnteredPrint = True;
 }
 
 static Bool EnteredTest1{False};
+
 void TestEnter() {
   EnteredTest1 = True;
 }
@@ -42,8 +46,9 @@ TEST(Instruct, Simple) {
   EXPECT_TRUE(EnteredTest1);
 }
 
-static Bool EnteredTest2{False};
-void TestParameters(Int i, Long l, Double d) {
+static Bool EnteredTest2 = False;
+
+extern "C" void TestParameters0(Int i, Long l, Double d) {
   EnteredTest2 = True;
 
   EXPECT_EQ(i, Int(10));
@@ -53,43 +58,185 @@ void TestParameters(Int i, Long l, Double d) {
   });
 }
 
-TEST(Instruct, Parameters) {
+extern "C" void TestParameters1(Int i1, Int i2, Int i3, Int i4, Int i5, Int i6) {
+  EnteredTest2 = True;
+
+  EXPECT_EQ(i1, Int(10));
+  EXPECT_EQ(i2, Int(10));
+  EXPECT_EQ(i3, Int(10));
+  EXPECT_EQ(i4, Int(10));
+  EXPECT_EQ(i5, Int(10));
+  EXPECT_EQ(i6, Int(10));
+}
+
+extern "C" void TestParameters2(Double i1, Double i2, Double i3, Double i4, Double i5, Double i6, Double i7, Double i8) {
+  EnteredTest2 = True;
+
+  EXPECT_EQ(i1, Double(10.0));
+  EXPECT_EQ(i2, Double(10.0));
+  EXPECT_EQ(i3, Double(10.0));
+  EXPECT_EQ(i4, Double(10.0));
+  EXPECT_EQ(i5, Double(10.0));
+  EXPECT_EQ(i6, Double(10.0));
+  EXPECT_EQ(i7, Double(10.0));
+  EXPECT_EQ(i8, Double(10.0));
+}
+
+extern "C" void TestParameters3(Int i1, Int i2, Int i3, Int i4, Int i5, Int i6, Int i7) {
+  EnteredTest2 = True;
+
+  EXPECT_EQ(i1, Int(10));
+  EXPECT_EQ(i2, Int(10));
+  EXPECT_EQ(i3, Int(10));
+  EXPECT_EQ(i4, Int(10));
+  EXPECT_EQ(i5, Int(10));
+  EXPECT_EQ(i6, Int(10));
+  EXPECT_EQ(i7, Int(10));
+}
+
+extern "C" void TestParameters4(Double i1, Double i2, Double i3, Double i4, Double i5, Double i6, Double i7, Double i8, Double i9) {
+  EnteredTest2 = True;
+
+  EXPECT_EQ(i1, Double(10.0));
+  EXPECT_EQ(i2, Double(10.0));
+  EXPECT_EQ(i3, Double(10.0));
+  EXPECT_EQ(i4, Double(10.0));
+  EXPECT_EQ(i5, Double(10.0));
+  EXPECT_EQ(i6, Double(10.0));
+  EXPECT_EQ(i7, Double(10.0));
+  EXPECT_EQ(i8, Double(10.0));
+  EXPECT_EQ(i9, Double(10.0));
+}
+
+TEST(Instruct, Parameters0) {
   Vector<Auto> parameters{};
+
+  parameters.clear();
+  EnteredTest2 = False;
 
   parameters.push_back(Base::Auto::As<Int>(10));
   parameters.push_back(Base::Auto::As<Long>(10));
   parameters.push_back(Base::Auto::As<Double>(10.0));
 
-  Base::Wrapping::Instruct((Void*)(TestParameters), parameters);
+  Base::Wrapping::Instruct((Void*)(TestParameters0), parameters);
+  EXPECT_TRUE(EnteredTest2);
+}
+
+
+TEST(Instruct, Parameters1) {
+  Vector<Auto> parameters{};
+
+  parameters.clear();
+  EnteredTest2 = False;
+
+  parameters.push_back(Base::Auto::As<Int>(10));
+  parameters.push_back(Base::Auto::As<Int>(10));
+  parameters.push_back(Base::Auto::As<Int>(10));
+  parameters.push_back(Base::Auto::As<Int>(10));
+  parameters.push_back(Base::Auto::As<Int>(10));
+  parameters.push_back(Base::Auto::As<Int>(10));
+  
+  Base::Wrapping::Instruct((Void*)(TestParameters1), parameters);
+  EXPECT_TRUE(EnteredTest2);
+}
+
+TEST(Instruct, Parameters2) {
+  Vector<Auto> parameters{};
+
+  parameters.clear();
+  EnteredTest2 = False;
+
+  parameters.push_back(Base::Auto::As<Double>(10));
+  parameters.push_back(Base::Auto::As<Double>(10));
+  parameters.push_back(Base::Auto::As<Double>(10));
+  parameters.push_back(Base::Auto::As<Double>(10));
+  parameters.push_back(Base::Auto::As<Double>(10));
+  parameters.push_back(Base::Auto::As<Double>(10));
+  parameters.push_back(Base::Auto::As<Double>(10));
+  parameters.push_back(Base::Auto::As<Double>(10));
+
+  Base::Wrapping::Instruct((Void*)(TestParameters2), parameters);
+  EXPECT_TRUE(EnteredTest2);
+}
+
+TEST(Instruct, Parameters3) {
+  Vector<Auto> parameters{};
+
+  parameters.clear();
+  EnteredTest2 = False;
+
+  parameters.push_back(Base::Auto::As<Int>(10));
+  parameters.push_back(Base::Auto::As<Int>(10));
+  parameters.push_back(Base::Auto::As<Int>(10));
+  parameters.push_back(Base::Auto::As<Int>(10));
+  parameters.push_back(Base::Auto::As<Int>(10));
+  parameters.push_back(Base::Auto::As<Int>(10));
+  parameters.push_back(Base::Auto::As<Int>(10));
+  
+  Base::Wrapping::Instruct((Void*)(TestParameters3), parameters);
+  EXPECT_TRUE(EnteredTest2);
+}
+
+
+TEST(Instruct, Parameters4) {
+  Vector<Auto> parameters{};
+
+  parameters.clear();
+  EnteredTest2 = False;
+
+  parameters.push_back(Base::Auto::As<Double>(10));
+  parameters.push_back(Base::Auto::As<Double>(10));
+  parameters.push_back(Base::Auto::As<Double>(10));
+  parameters.push_back(Base::Auto::As<Double>(10));
+  parameters.push_back(Base::Auto::As<Double>(10));
+  parameters.push_back(Base::Auto::As<Double>(10));
+  parameters.push_back(Base::Auto::As<Double>(10));
+  parameters.push_back(Base::Auto::As<Double>(10));
+  parameters.push_back(Base::Auto::As<Double>(10));
+
+  Base::Wrapping::Instruct((Void*)(TestParameters4), parameters);
   EXPECT_TRUE(EnteredTest2);
 }
 
 #if defined(__GNUC__) && !defined(__clang__)
+static Bool EnteredTest3 = False;
+
 #ifdef USE_PYTHON
 PY_MODULE(CTest) {
   Procedure("Print", Print);
+  EnteredTest3 = True;
 }
 
 TEST(PyWrapping, Simple) {
+  EnteredTest3 = False;
+  EnteredPrint = False;
+
   EXPECT_NO_THROW({
     EXPECT_TRUE(Wrapping::Create(new PY_MODULE_CLI(CTest)));
   });
   EXPECT_TRUE(Wrapping::Test("Python", "CTest", "Print"));
+  EXPECT_TRUE(EnteredTest3);
+  EXPECT_TRUE(EnteredPrint);
 }
 #endif
 
 #ifdef USE_RUBY
 RUBY_MODULE(CTest) {
   Procedure("Print", Print);
+  EnteredTest3 = True;
 }
 
 TEST(RubyWrapping, Simple) {
+  EnteredTest3 = False;
+  EnteredPrint = False;
+
   EXPECT_TRUE(Wrapping::Test("Ruby", "CTest", "Print"));
+  EXPECT_TRUE(EnteredTest3);
+  EXPECT_TRUE(EnteredPrint);
 }
 #endif
 #endif
 
 int main(){
-  Base::Log::Level() = EDebug;
   return RUN_ALL_TESTS();
 }
