078bec5205d44fc3a0758e634e144261e3ed1632
Hung Nguyen Xuan Pham <hung0913208@gmail.com>
2019-12-26 20:22:55 +0700

    [FIX][Base] test case causes memory leak
    Problems:
    - Test case Protect cause memory leak and fail after that
    
    Solution:
    - Improve test case

--- Tests/Base/Protect.cc
+++ Tests/Base/Protect.cc
@@ -9,7 +9,10 @@ TEST(Protect, Execuable) {
   auto address = ABI::Calloc(10, 1);
   auto codes = PROT_EXEC|PROT_READ|PROT_WRITE;
 
+  ASSERT_NEQ(address, None);
   EXPECT_EQ(Base::Protect(address, 10, codes), ENoError);
+
+  ABI::Free(address);
 }
 
 int main() {
