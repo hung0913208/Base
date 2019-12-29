08b549ea9bc78e747618187c06bdb33284f90d86
Pham Nguyen Xuan Hung <hung0913208@gmail.com>
2019-12-29 10:42:34 +0700

    [ENHANCE][Tools/Utilities] improve leaking detect
    Description:
    - Sometime, the memory leak doesn't show any abnormal issue on error
    pages and we should check if the memory is released completedly at the
    exit or not manually.
    
    Change-Id: If9716dd64abf0c1fe8f41068f014e2f30c3558a2
    Signed-off-by: Pham Nguyen Xuan Hung <hung0913208@gmail.com>

--- Tools/Utilities/valgrind.sh
+++ Tools/Utilities/valgrind.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 exec 5>&1
-output=$( valgrind -v --tool=memcheck --leak-check=full --leak-resolution=high --track-origins=yes --show-leak-kinds=all --error-exitcode=-1 $@ 2>&1 | tee /dev/fd/2 )
+output=$( valgrind -v --tool=memcheck --leak-check=full --leak-resolution=high --track-origins=yes --show-leak-kinds=all --error-exitcode=-1 --trace-children=yes $@ 2>&1 | tee /dev/fd/2 )
 exit_code_1=$?
 
 if [[ ! $(grep "ERROR SUMMARY: 0 errors" <<< "$output") ]] || [[ $exit_code_1 != 0 ]]; then
@@ -16,6 +16,8 @@ if [[ ! $(grep "ERROR SUMMARY: 0 errors" <<< "$output") ]] || [[ $exit_code_1 !=
 		echo "Please check your code again, still have memory leak"
 		exit_code_1=2
 	fi
+elif [[ ! $(grep "in use at exit: 0 bytes in 0 blocks" <<< "$output") ]]; then
+	echo "It seems you have memory leak with the child processes"
 fi
 
 if [ $exit_code_1 != 0 ]; then
