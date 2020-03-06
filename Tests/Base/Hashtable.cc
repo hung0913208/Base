a4e9e92702f062be6b859d30d7c7ae4e76fa140e
Pham Nguyen Xuan Hung <hung0913208@gmail.com>
2020-03-06 22:21:43 +0700

    [UPGRADE][Base] test Hashtable with random values
    Description:
    - Add new test case to verify putting random value with provided index
    
    Change-Id: I5012a1c87c77d6f14fe4662f58620e20ffbe0e46
    Signed-off-by: Pham Nguyen Xuan Hung <hung0913208@gmail.com>

--- Tests/Base/Hashtable.cc
+++ Tests/Base/Hashtable.cc
@@ -1,36 +1,101 @@
 #include <Hashtable.h>
 #include <Unittest.h>
+#include <Utils.h>
+#include <cstdlib>
 
 #define MAX_SIZE 1000
 
-TEST(Hashtable, Simple) {
-  for (UInt size = 1; size < MAX_SIZE; ++size) {
-    Base::Hashtable<UInt, UInt> int1{size,
-      [](UInt* value) -> Int{ return *value; }
-    };
+TEST(HashtableV1, Simple) {
+  auto perform = [&]() {
+    for (UInt size = 1; size < MAX_SIZE; ++size) {
+      Base::Hashtable<UInt, UInt> int1{size,
+        [](UInt* value) -> Int{ return (*value); }
+      };
 
-    for (UInt index = 0; index < size; ++index) {
-      int1.Put(index, index);
+      for (UInt index = 0; index < size; ++index) {
+        EXPECT_EQ(int1.Put(index, index), ENoError);
+      }
+
+      for (UInt index = 0; index < size; ++index) {
+        EXPECT_EQ(int1.Get(RValue(index)), index);
+      }
     }
-
-    for (UInt index = 0; index < size; ++index) {
-      EXPECT_EQ(int1.Get(RValue(index)), index);
-    }
-  }
-}
-
-TEST(Hashtable, LevelUp){
-  Base::Hashtable<UInt, UInt> int1{5,
-    [](UInt* value) -> Int{ return *value; }
   };
 
-  for (UInt index = 0; index < 10; ++index) {
-    int1.Put(index, index);
-  }
-
-  for (UInt index = 0; index < 10; ++index) {
-    EXPECT_EQ(int1.Get(RValue(index)), index);
-  }
+  TIMEOUT(50, { perform(); });
 }
 
-int main() { RUN_ALL_TESTS(); }
+TEST(HashtableV1, Random) {
+  auto perform = [&]() {
+    Base::Pair<UInt, UInt> mapping[MAX_SIZE];
+
+    for (UInt size = 1; size < MAX_SIZE; ++size) {
+      Base::Hashtable<UInt, UInt> int1{size,
+        [](UInt* key) -> Int{ return (*key); }
+      };
+
+      for (UInt i = 0; i < size; ++i) {
+        mapping[i].Left = rand();
+        mapping[i].Right = rand();
+
+        EXPECT_EQ(int1.Put(mapping[i].Left, mapping[i].Right), ENoError);
+      }
+    
+      for (UInt i = 0; i < size; ++i) { 
+        EXPECT_EQ(int1.Get(mapping[i].Left), mapping[i].Right);
+      }
+    }
+  };
+
+  TIMEOUT(50, { perform(); });
+}
+
+TEST(HashtableV1, LevelUp){
+  auto perform = [&]() {
+    Base::Pair<UInt, UInt> mapping[MAX_SIZE];
+
+    for (UInt level = 1; level < MAX_SIZE/5; ++level) {
+      Base::Hashtable<UInt, UInt> int1{5,
+        [](UInt* value) -> Int{ return *value; }
+      };
+    
+      for (UInt i = 0; i < 5*level; ++i) {
+        mapping[i].Left = rand();
+        mapping[i].Right = rand();
+
+        EXPECT_EQ(int1.Put(mapping[i].Left, mapping[i].Right), ENoError);
+      }
+
+      for (UInt i = 0; i < 5*level; ++i) { 
+        EXPECT_EQ(int1.Get(mapping[i].Left), mapping[i].Right);
+      }
+    }
+  };
+
+  TIMEOUT(80, { perform(); });
+}
+
+TEST(HashtableV1, PutGet) {
+  auto perform = [&]() {
+    for (UInt size = 1; size < MAX_SIZE; ++size) {
+      Base::Hashtable<UInt, UInt> int1{size,
+        [](UInt* key) -> Int{ return *key; }
+      };
+
+      for (UInt i = 0; i < size; ++i) {
+        EXPECT_EQ(int1.Put(Base::Pair<UInt, UInt>{i*size, i}), ENoError);
+      }
+   
+      for (UInt i = 0; i < size; ++i) { 
+        EXPECT_EQ(int1.Get(i*size), i);
+      }
+    }
+  };
+
+  TIMEOUT(200, { perform(); });
+}
+
+int main() { 
+  Base::Log::Level() = EDebug;
+  return RUN_ALL_TESTS(); 
+}
