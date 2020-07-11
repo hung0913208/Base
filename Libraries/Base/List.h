dd1339243d3d4671da18c5773808de1b28379678
Hung Nguyen Xuan Pham <hung0913208@gmail.com>
2020-07-11 18:16:18 +0700

    [UPGRADE][Base] improve List's performance
    Description:
    - Apply new data structure to class List to improve performance and
    reduce unexpected issues.
    
    Signed-off-by: Hung Nguyen Xuan Pham <hung0913208@gmail.com>

--- Libraries/Base/List.h
+++ Libraries/Base/List.h
@@ -17,53 +17,139 @@ void DumpWatch(String parameter);
 } // namespace Debug
 
 class List {
- private:
-  enum ActionE { ERead = 0, ELModify, ERModify, ERemove, EEnd };
-  using Barrier = Pair<ULong*, Mutex>;
+ protected:
+  /* @NOTE: this enum defines how many action we can do with node. Normally.
+   * we must try to acquire the node before doing anything. This is nature
+   * way since the List is designed to handle with highload system where 
+   * there are so many threads race to gain the right to control nodes. So
+   * the control flow should be like this:
+   *
+   *            EFree ----> EAcquired ----> <Action> ----> EFree
+   *                           |                             ^
+   *                           |                             |
+   *                           +-----------------------------+
+   * ______________________________________________________________________ */
 
- public:
-  List(Int parallel = 0);
-  ~List();
-
-  template<typename T> 
-  T* At(ULong position, Bool use_index = False) {
-    Item* item = Access(ERead, position, !use_index);
-    T* result = (T*) (item? item->_Ptr : None);
-
-    if (Unlock(item)) {
-      return result;
-    } else {
-      return None;
-    }
-  }
-
-  ULong Size();
-  ULong Add(Void* ptr);
-  Bool  Del(ULong index, Void* ptr);
-
- private:
-  struct Item {
-    Void* _Ptr;
-    ULong _Index;
-    Item *_Next, *_Prev;
-
-    Item(): _Ptr{None}, _Index{0}, _Next{None}, _Prev{None} {}
+  enum ActionE {
+    EFree = 0,
+    EDelete = 1,
+    EModify = 2,
+    EAcquired = 3,
+    EDetached = 4,
   };
 
-#if DEBUGING
+ public:
+#if !NDEBUG
+  explicit List(Int retry = -1, Bool dumpable = False);
+#else
+  explicit List(Int retry = -1);
+#endif
+  virtual ~List();
+
+  /* @NOTE: this method is used to get the pointer of a node, base on index or
+   * its position number */
+  template<typename T> 
+  T* At(ULong number, Bool is_index = False) {
+    T* result = None;
+    Long nsec = 1;
+
+    for (auto retry = _Retry; retry > 0; --retry) {
+      Node* node = None;
+
+      if ((node = Access(EModify, number, is_index))) {
+        result = (T*)node->Ptr;
+
+        Release(node);
+        break;
+      } 
+
+      Idle((nsec = ((nsec * 2) % ULong(1e9))));
+    }
+
+    return result;
+  }
+
+  /* @NOTE: these methods is used to interact directly with our list to add or
+   * delete pointers from it */
+  Long Add(Void* pointer, Int retry = -1);
+  Bool Del(ULong index, Void* pointer, ErrorCodeE* code = None);
+
+  /* @NOTE: this method is used to acquire a node to prevent deleting actions */
+  Bool Acquire(ULong index);
+
+  /* @NOTE: this method is used to release a node out from state acquired */
+  Bool Release(ULong index);
+
+  /* @NOTE: */
+  ULong Size();
+
+ protected:
+  struct Node {
+    /* @NOTE: this is how our node is defined so with it, we can build a list to
+     * work on parallel */
+
+    Void *Ptr;
+    Node *Next, *Prev, **PNext, **PPrev, **PHead, **PLast;
+    ULong Index;
+    ActionE State;
+
+    explicit Node(Void* pointer, Node** phead, Node** plast) {
+      Ptr = pointer;
+      Next = None;
+      Prev = None;
+      PPrev = None;
+      PNext = None;
+      PHead = phead;
+      PLast = plast;
+      State = EFree;
+      Index = 0;
+    }
+  };
+
+  /* @NOTE: this method is used to attach a node into our list, the node could
+   * be defined some-where else, waiting to be added into our list */
+  Long Attach(Node* node);
+
+  /* @NOTE: this method is used to detach a node out from our list so we could
+   * modify it freely. To make sure that the node is within our list, the index
+   * should be provided */
+  Node* Detach(ULong index);
+
+  /* @NOTE: these methods are used to access a node to grand an action to it
+   * so we could control the behavior of each node on parallel */
+  Node* Access(ActionE action, Node* node);
+  Node* Access(ActionE action, ULong &number, Bool is_index = False,
+               ErrorCodeE* code = None);
+
+  /* @NOTE: this method is used to acquire a priviledge to access the node,
+   * this input is a tricky way to prenvent node to be deleted during request
+   * priviledge */
+  Bool Acquire(Node** node);
+
+  /* @NOTE: these methods are used to release the acquired state from the node
+   * so it could be handle another requests */
+  Bool Release(Node* node, ActionE from);
+  Bool Release(Node* node);
+
+ private:
+  static void Idle(Long nanosec);
+
+  /* @NOTE: this method is used to detach a node out from our list so we could
+   * modify it freely. To make sure that the node is within our list, the index
+   * should be provided */
+  Node* Detach(Node* node);
+
+  /* @NOTE: this function is used only when we need to debug watch-stopper
+   * issues and we need to list everything inside the list */
   friend void Base::Debug::DumpWatch(String parameter);
+
+#if !NDEBUG
+  Bool _Dumpable;
 #endif
 
-  ULong Add(Item* prev, Void* ptr);
-  Item* Access(ActionE action, Item* node);
-  Item* Access(ActionE action, ULong index, Bool counting = False);
-  Bool Unlock(Item* item);
-  Bool Unlock(ULong ticket);
-
-  Bool _Global, _Sweep;
-  Item *_Head, *_Curr;
-  Barrier* _Barriers;
-  ULong _Size[2], _Parallel, _Count;
+  UInt _Retry, _Lock;
+  Node *_Head, *_Last;
+  ULong _Size, _Count;
 };
 } // namespace Base
 #endif
--- Libraries/Base/Source/List.cc
+++ Libraries/Base/Source/List.cc
@@ -3,314 +3,461 @@
 #include <Logcat.h>
 #include <Vertex.h>
 
+#include <iostream>
 #include <thread>
 
+using TimeSpec = struct timespec;
+
 namespace Base {
-List::List(Int parallel): _Global{False}, _Sweep{False}, _Head{None},
-      _Curr{None}, _Parallel{0}, _Count{0} {
-  if (parallel <= 0) {
-    parallel = std::thread::hardware_concurrency();
-  }
+namespace Internal {
+void Idle(TimeSpec* spec);
+Int Random(Int min, Int max);
+} // namespace Internal
 
-  _Barriers = new Barrier[3 * parallel];
-  _Size[1] = 3 * parallel;
-  _Size[0] = 0;
-  
-  for (auto i = 0; i < 3 * parallel; ++i) {
-    _Barriers[i].Left = new ULong[EEnd];
+#if NDEBUG
+List::List(Int retry) {
+#else
+List::List(Int retry, Bool dumpable) {
+  _Dumpable = dumpable;
+#endif
+  _Count = 0;
+  _Size = 0;
+  _Head = new Node{None, &_Head, &_Last};
+  _Last = new Node{None, &_Head, &_Last};
 
-    ABI::Memset(_Barriers[i].Left, 0, EEnd * sizeof(ULong));
-    MUTEX(&_Barriers[i].Right);
+  _Head->Next = _Last;
+  _Last->Prev = _Head;
+
+  /* @NOTE: here is the explanation of how the nodes connect each-other:
+   * - We have 2 node A and B, A->Next --> B while B->Prev --> A while
+   *   A->PNext --> &(B->Next) and B->PPrev --> &(A->Prev). For more detail
+   *   please check the image below.
+   *
+   *      +-------------+     +-------------+
+   *      |      A      |     |      B      |
+   *      |             |     |             |
+   *  ----|Prev&        |<----|Prev&        |
+   *      |    ^        |     |    ^        |
+   *  --->|    |   &Next|---->|    |   &Next|----
+   *      |    ++  ^    |     |    ++  ^    |
+   *  -------+  |  +-------------+  |  +---------
+   *      |  |  |       |     |  |  |       |
+   *      |PNext|  PPrev|     |PNext|  PPrev|
+   *      +-----|----|--+     +-----|----|--+
+   *            |    |              |    |
+   *  ----------+    +--------------+    +-------
+   *
+   * - With this designation, it can be easily to remove a node on parallel
+   *   using our atomic operators. To do that, we just use MCOPY to write Next
+   *   to PNext and Prev to PPrev. Doing that would cause rewrite the parameters
+   *   Next of the previous node and Prev of the next node respectively and the
+   *   middle node will be detached before we do removing it.
+   * ------------------------------------------------------------------------ */
+
+  _Head->PPrev = &_Last->Prev;
+  _Last->PNext = &_Head->Next;
+
+  if (retry > 0) {
+    _Retry = retry;
+  } else {
+    _Retry = 10*std::thread::hardware_concurrency();
   }
 }
 
 List::~List() {
-  _Sweep = True;
+  /* @NOTE: this destructor is unsafe since it's call by os and we can't
+   * control it. The user should know that their thread should be done 
+   * before this method is called. Everything is handled easily by WatchStopper
+   * but i can't control daemon threads since they are out of my scope */
 
-  for (auto item = _Curr; item;) {
-    auto tmp = item;
+  Node* node{_Head};
 
-    item = item->_Prev;
-    delete tmp;
+  for (ULong i = 0; i < _Size; ++i) {
+    Node* temp{node};
+
+    node = node->Next;
+
+    if (temp != _Head && temp != _Last) {
+      delete temp;
+    }
   }
 
-  for (auto i = _Size[1]; i > 0; --i) {
-    delete [] _Barriers[i - 1].Left;
-  }
-
-  delete [] _Barriers;
+  delete _Head;
+  delete _Last;
 }
 
-ULong List::Size() { return _Size[0]; }
+void List::Idle(Long nanosec) {
+  TimeSpec spec{.tv_sec=0, .tv_nsec=nanosec};
+    
+  Internal::Idle(&spec);
 
-ULong List::Add(Void* ptr) {
-  return Add(_Curr, ptr);
 }
 
-ULong List::Add(Item* prev,  Void* ptr) { 
-  ULong index{INC(&_Count)};
-  Item* next{prev ? prev->_Next : None};
-  Item* item{new Item{}};
-  Item* curr{_Curr};
+ULong List::Size() { return _Size; }
 
-  if (_Sweep) {
-    return False;
-  }
+Long List::Add(Void* pointer, Int retry) {
+  Node* node{new Node(pointer, &_Head, &_Last)};
+  Long nsec{1};
 
-  item->_Index = index;
-  item->_Ptr = ptr;
+  do {
+    ULong result = Attach(node);
 
-  if (!_Sweep) {
-    Bool done{False};
-
-    if (Access(ELModify, prev)) {
-      prev->_Next = item;
-      item->_Prev = prev;
-    }
-
-    if (Access(ERModify, next)) {
-      next->_Prev = item;
-      item->_Next = prev;
-    }
-
-    do {
-      if ((done = ACK(&_Global, True))) {
-        CMPXCHG(&_Head, None, item);
-
-	if (!CMPXCHG(&_Curr, None, item)) {
-          if (!next && !CMPXCHG(&_Curr, curr, item)) {
-            curr = _Curr;
-            item->_Prev = curr;
-            curr->_Next = item;
-          }
-        }
-
-        INC(&_Size[0]);
-        REL(&_Global);
-      }
-
-      BARRIER();
-    } while(!done);
-
-    if (prev) {
-      Unlock(prev);
-    }
-
-    if (next) {
-      Unlock(next);
-    }
-  }
-
-  return item->_Index;
-}
-
-Bool List::Del(ULong expected, Void* ptr) {
-  Item* curr{None};
-  Bool result{False};
-
-  if (_Sweep) {
-    return False;
-  }
-
-  curr = Access(ERemove, expected);
-  result = curr && curr->_Ptr == ptr;
-
-  if (!_Sweep && result) {
-    Item* prev{Access(ERModify, curr->_Prev)};
-    Item* next{Access(ELModify, curr->_Next)};
-    Bool done{False};
-
-    if (prev) {
-      prev->_Next = next;
-    }
-
-    if (next) {
-      next->_Prev = prev;
-    }
-
-    if (prev && !Unlock(prev)) {
-      Bug(EBadAccess, Format{"Can unlock node {}"}.Apply(prev->_Index));
-    }
-
-    if (next && !Unlock(next)) {
-      Bug(EBadAccess, Format{"Can unlock node {}"}.Apply(next->_Index));
-    }
-
-    if (_Head == curr) {
-      do {
-        if ((done = ACK(&_Global, True))) {
-          CMPXCHG(&_Head, curr, curr->_Next);
-          REL(&_Global);
-        }
-
-        BARRIER();
-      } while(!done);
-    }
-
-    if (_Curr == curr) {
-      do {
-        if ((done = ACK(&_Global, True))) {
-          CMPXCHG(&_Curr, curr, curr->_Prev);
-          REL(&_Global);
-        }
-
-        BARRIER();
-      } while(!done);
-    }
-
-    DEC(&_Size[0]);
-  }
-
-  if (result) {
-    delete curr;
-  }
-
-  if (curr) {
-    if (Unlock(expected)) {
+    if (result) {
+      INC(&_Size);
       return result;
-    } else if (_Curr && _Head) {
-      Bug(EBadAccess, Format{"Can delete node {}"}.Apply(expected));
     }
+
+    if (retry > 0) {
+      retry--;
+    }
+
+    /* @NOTE: normally, we don't need this solution to cooldown our CPU but i
+     * put it here to make everything slow abit so the CPU will have enough
+     * time to handle everything with the hope that adding will work if we wait
+     * enough time, when everything slow enough */
+
+    List::Idle((nsec = (nsec * Internal::Random(1, 10) % ULong(1e9))));
+  } while (retry != 0);
+
+  delete node;
+
+#if !NDEBUG
+  if (_Dumpable) {
+    ABI::KillMe();
+  }
+#endif
+
+  return 0;
+}
+
+Long List::Attach(Node* node) {
+  Int retry = _Retry;
+  ULong result = 0;
+
+  /* @NOTE: as i have said before, the node could be detached and reattach again
+   * using this function. During this time, the node's index is defined so we 
+   * don't need to check it. We should make sure that its head and last are same
+   * with our list to make sure it's created by our list. */
+
+  if (node->Index == 0) {
+    node->Index = INC(&_Count);
+  } else if (node->PHead != &_Head) {
+    return -1; // <-- We detect a wrong node which isn't created by this list
+  }
+
+  result = node->Index;
+
+  /* @NOTE: request adding our node to the end of our list, this should be done
+   * after several attempts or we should return 0 so we could request user wait
+   * a bit more since the CPU is on highload and couldn't handle our request 
+   * gently in a limit time */
+
+  while (retry > 0) {
+    Node* last = node;
+
+    retry--;
+
+    /* @NOTE: here is the theory of how to build this list with the RCU 
+     * mechanism:
+     *
+     * +------+  +------+
+     * | head |  | last | <- While this guy will become the normal node
+     * +------+  +------+
+     *   ^  |      ^  |  
+     *   |  |      |  |     +------+
+     *   |  v      |  |     | wait | <- this guy will become the `last` node
+     * ........    |  |     +------+
+     *   ^  |      |  |
+     *   |  |      |  |
+     *   |  v      |  |
+     * +------+    |  |
+     * | node | ---+  |
+     * +------+       |
+     *   ^            |
+     *   |            |
+     *   +------------+
+     *
+     * - The node `wait` will become the new `last` node while the old `last`
+     *   node will turn into a normal node with small swaping here. This would
+     *   make everything run faster while maintaining the chain stable.
+     * - The _Head and _Last can't be deleted so we could use them as starting
+     *   point easily. They will be deleted by the List's destructor only
+     *
+     * ---------------------------------------------------------------------- */
+
+    /* @NOTE: build our new `last` node on parallel, since it's still a local
+     * node so we don't need to care anyone touch it when we are prepare this
+     * node  */
+
+    last->Index = 0;
+    last->Next = None;
+    last->Prev = _Last;
+    last->PPrev = None;
+    last->PNext = &_Last->Next;
+      
+    if (CMPXCHG(&_Last->Index, 0, result)) {
+      Node* curr = _Last;
+
+      /* @NOTE: replicate data of our new node to the current `last` node so we
+       * could easy control adding new node to our list while keep our chain
+       * stable, even if we are dealing with the high-loaded systems */
+
+      _Last->Ptr = last->Ptr;
+      _Last->Next = last;
+      _Last->PPrev = &last->Prev; 
+
+      /* @NOTE: everything is done now, we instruct to every node to know that
+       * now we are applying the new `last` node while the old one will be used
+       * as a normal node */
+
+      if (!CMPXCHG(&_Last, curr, last)) {
+        Bug(EBadLogic, "Can't announce a new `last` node");
+      }
+    } else {
+      goto again;
+    }
+
+    return result;
+
+again:
+    BARRIER();
   }
 
   return 0;
 }
 
-List::Item* List::Access(ActionE action, ULong index, Bool counting) {
-  Item* node{None};
+List::Node* List::Detach(Node* node) {
+  if (node && !CMP(&node, _Head) && !CMP(&node, _Last)) {
+    auto prev = node->Prev;
+    auto next = node->Next;
+    auto pprev = node->PPrev;
+    auto pnext = node->PNext;
 
-  if (counting) {
-    Vertex<void> escaping{[&](){ while(!ACK(&_Global, True)) { BARRIER(); } },
-                          [&](){ REL(&_Global); }};
+    /* @NOTE: edit the next pointer of the previous node using pnext so we 
+     * could optimize performance while keep everything safe */
 
-    for (node = _Head; node; node = node->_Next) {
-      if ((--index) == 0) {
-        break;
-      }
+    if (next) {
+      MCOPY(pnext, &next, sizeof(next));
     }
-  } else {
-    Vertex<void> escaping{[&](){ while(!ACK(&_Global, True)) { BARRIER(); } },
-                          [&](){ REL(&_Global); }};
 
-    for (node = _Head; node; node = node->_Next) {
-      if (node->_Index == index) {
-        break;
+    if (prev) {
+      MCOPY(pprev, &prev, sizeof(prev));
+    }
+
+    next->PNext = pnext;
+    prev->PPrev = pprev;
+
+    Release(node);
+  }
+
+  return node;
+}
+
+List::Node* List::Detach(ULong index) {
+  Node* node = None;
+  Long nsec = 1;
+
+  for (auto retry = _Retry; retry > 0; --retry) {
+    if ((node = Access(EDelete, index, true))) {
+      return Detach(node);
+      break;
+    } 
+
+    Idle((nsec = (nsec * Internal::Random(1, 10) % ULong(1e9))));
+  }
+
+  return None;
+}
+
+Bool List::Del(ULong index, Void* ptr, ErrorCodeE* code) {
+  Node *node = None;
+  Long nsec = 1;
+
+  for (auto retry = _Retry; retry > 0; --retry) {
+    node = None;
+
+    if ((node = Access(List::EDelete, index, True, code))) {
+      if (ptr == node->Ptr) {
+        if (!Detach(node)) {
+          if (code) {
+            *code = BadAccess("can't detach node as it should be").code();
+          }
+
+          goto reattach;
+        }
+
+        DEC(&_Size);
+        delete node;
+        return True;
+      } else {
+        Release(node);
       }
+
+      break;
+    } else {
+      Idle((nsec = ((nsec * Internal::Random(2, 10)) % ULong(1e9))));
     }
   }
 
-  return Access(action, node);
+fail:
+#if !NDEBUG
+  if (_Dumpable) {
+    ABI::KillMe();
+  }
+#endif
+
+  return False;
+
+reattach:
+  Release(node);
+
+  while (UNLIKELY(Attach(node), Long(node->Index))) {
+    Idle((nsec = (nsec * Internal::Random(1, 10) % ULong(1e9))));
+  }
+
+  goto fail;
 }
 
-List::Item* List::Access(ActionE action, Item* node) {
-  Item* prev{node? node->_Prev: None};
-  Item* next{node? node->_Next: None};
-  Item* result{None};
-  UInt i;
+List::Node* List::Access(List::ActionE action, ULong& number, Bool is_index,
+                         ErrorCodeE* code) {
+  Node *node{_Head};
+  Node *next{None};
+  Bool touched{False};
 
-  if (!node) {
+  /* @NOTE: acquire our head node first so we could prevent access to deadly
+   * and cause crash to application */
+
+  if (action == EFree) {
     return None;
   }
 
-  for (i = 0; i < _Size[1]; ++i) {
-    switch (action) {
-    default:
-      break;
+  while ((!is_index && number > 0) || (is_index && number != node->Index)) {
+    /* @NOTE: try to consult the permision to the next node within a certain
+     * attempts, hopfully we can gain it before the*/
 
-    case ERead:
-      if (CMP(&_Barriers[i].Left[ERemove], node->_Index)) {
-        goto finish;
-      }
-      break;
+    for (Int retry = _Retry; retry > 0; --retry) {
+      if (node->Next && !CMP(&node->Next, _Last)) {
+        /* @NOTE: try to acquire the next one, we are inquired to do that
+         * so we could gain the priviledge to manipulate this node before
+         * the timeout excess */
 
-    case ERModify:
-      if (CMP(&_Barriers[i].Left[ELModify], node->_Index)) {
-        goto checking;
-      } else if (CMP(&_Barriers[i].Left[ERModify], node->_Index)) {
-        goto pending;
-      }
-      break;
+        if (Acquire(&(node->Next))) {
+          goto done_1;
+        } else {
+          BARRIER();
+        }
+      } else {
+        if (code) {
+          *code = NotFound("Reach end of list but can't see your node").code();
+        }
 
-    case ELModify:
-      if (CMP(&_Barriers[i].Left[ERModify], node->_Index)) {
-        goto checking;
-      } else if (CMP(&_Barriers[i].Left[ELModify], node->_Index)) {
-        goto pending;
-      }
-      break;
-
-    case ERemove:
-      if (CMP(&_Barriers[i].Left[ERemove], node->_Index)) {
-        goto finish;
-      } else if (prev && CMP(&_Barriers[i].Left[ERModify], prev->_Index)) {
-        goto finish;
-      } else if (next && CMP(&_Barriers[i].Left[ELModify], next->_Index)) {
-        goto finish;
-      }
-      break;
-    } 
-  }
-
-checking:
-  if (_Size[1] <= _Parallel) {
-    while (!ACK(&_Global, True)) {
-      BARRIER();
-    }
-  }
-  
-  for (i = 0; i < _Size[1]; ++i) {
-    Int j = 0;
-
-    for (; j < EEnd; ++j) {
-      if (!CMP(&_Barriers[i].Left[j], 0)) {
-        break;
+        goto fail_1;
       }
     }
 
-    if (j == EEnd) {
-      break;
+    /* @NOTE: unluckily, we reachs the timeout now so we have to drop it here
+     * and return the current node so the higher level function can use it
+     * for the next attempts */
+
+    for (auto retry = _Retry; retry > 0; --retry) {
+      if (Release(node, EAcquired)) {
+        goto done_2;
+      }
+    }
+
+    Bug(EBadAccess, Format{"state shouldn't be {}"}.Apply(node->State));
+
+done_2:
+    if (code) {
+      *code = DoAgain("Can't acquire the next node").code();
+    }
+
+    return None;
+
+done_1:
+    /* @NOTE: got it, i have catched the right permission to do with the 
+     * next node so we will release the current node to */
+
+    next = node->Next;
+    touched = True;
+
+    if (Release(node, EAcquired)) {
+      node = next;
+    } else {
+      /* @NOTE: we don't expect this block happen since we are the owner of
+       * this node recently so we should have fully priviledge to this node
+       * and we must be the only one to be able to release this node or we
+       * are facing race condition some-where else */
+
+      if (code) {
+        *code = BadAccess("Can't release node as it should be").code();
+      }
+
+      goto fail_1;
+    }
+ 
+    if (!is_index) {
+      --number;
     }
   }
 
-  LOCK(&_Barriers[i].Right);
-  INC(&_Parallel);
-  goto registering;
+  /* @NOTE: we have the right node recently so now able update the real
+   * priviledge of this node */
 
-pending:
-  LOCK(&_Barriers[i].Right);
+  return Access(action, node);
 
-registering:
-  XCHG64(&_Barriers[i].Left[action], node->_Index);
-  result = node;
+fail_1:
+  if (node != _Head) {
+    for (auto retry = _Retry; retry > 0; --retry) {
+      if (Release(node, EAcquired)) {
+        goto fail_2;
+      }
+    }
+      
+    Bug(EBadAccess, "can't release node from state EAcquired");
+  }
 
-finish:
-  return result;
+fail_2:
+  return None;
 }
 
-Bool List::Unlock(ULong index) {
-  Bool locked = _Global && _Parallel >= _Size[1];
-  Bool result = False;
+List::Node* List::Access(List::ActionE action, Node* node) {
+  switch(action) {
+  default:
+    node->State = action;
 
-  for (ULong i = 0; i < _Size[1] && !result; ++i) {
-    for (Int j = 0; j < Int(EEnd) && !result; ++j) {
-      if ((result = CMPXCHG(&_Barriers[i].Left[j], index, 0))) {
-        DEC(&_Parallel);
-        UNLOCK(&_Barriers[i].Right);
-      }
-    }
+  case EAcquired:
+    break;
   }
 
-  if (result && locked) {
-    _Global = False;
-  }
-  return result;
+  return node;
 }
 
-Bool List::Unlock(Item* item) {
-  if (item) {
-    return Unlock(item->_Index);
+Bool List::Acquire(Node** node) {
+  if (node != &_Head && node != &_Last) {
+    return CMPXCHG(&(*node)->State, EFree, EAcquired);
   } else {
+    return True;
+  }
+}
+
+Bool List::Release(Node* node) {
+  ActionE state{node? node->State: EFree};
+
+  if (node == _Head || node == _Last) {
+    return True;
+  } else if (state == EFree) {
     return False;
+  } else {
+    return CMPXCHG(&node->State, state, EFree);
+  }
+}
+
+Bool List::Release(Node* node, List::ActionE state) {
+  if (node != _Head && node != _Last) {
+    return CMPXCHG(&node->State, state, EFree);
+  } else {
+    return True;
   }
 }
 } // namespace Base
